17b8f4aa600d235d6a885dc270de858bff7854d0
Konstantin8105 <konstantin8105@gmail.com>
2018-11-06 11:51:00 +0300

    test: add tests

--- pe_test.go
+++ pe_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -60,6 +61,7 @@ func TestEditor(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
+			E = editorConfig{}
 			E.filename = out.Name()
 			out.Close()
 
@@ -86,9 +88,46 @@ func TestEditor(t *testing.T) {
 					t.Fatal(err)
 				}
 				if !bytes.Equal(b1, b2) {
-					t.Fatalf("is not same")
+					t.Fatalf(ShowDiff(string(b1), string(b2)))
 				}
 			}
 		})
 	}
+}
+
+// ShowDiff will print two strings vertically next to each other so that line
+// differences are easier to read.
+func ShowDiff(a, b string) string {
+	aLines := strings.Split(a, "\n")
+	bLines := strings.Split(b, "\n")
+	maxLines := int(math.Max(float64(len(aLines)), float64(len(bLines))))
+	out := "\n"
+
+	for lineNumber := 0; lineNumber < maxLines; lineNumber++ {
+		aLine := ""
+		bLine := ""
+
+		// Replace NULL characters with a dot. Otherwise the strings will look
+		// exactly the same but have different length (and therfore not be
+		// equal).
+		if lineNumber < len(aLines) {
+			aLine = strconv.Quote(aLines[lineNumber])
+		}
+		if lineNumber < len(bLines) {
+			bLine = strconv.Quote(bLines[lineNumber])
+		}
+
+		diffFlag := " "
+		if aLine != bLine {
+			diffFlag = "*"
+		}
+		out += fmt.Sprintf("%s %3d %-40s%s\n", diffFlag, lineNumber+1, aLine, bLine)
+
+		if lineNumber > len(aLines) || lineNumber > len(bLines) {
+			out += "and more other ..."
+			break
+		}
+	}
+
+	return out
 }
